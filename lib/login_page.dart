a3337b36d43144e60af5465e18c07ee0070179b7
KechEDD <86821454+kechprog@users.noreply.github.com>
2022-10-22 11:19:37 -0400

    initial coommit

---
+++ lib/login_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  String login, pasword;
+  LoginPage({Key? key, required this.login, required this.pasword}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
